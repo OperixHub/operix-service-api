36c19ac8d368006fecb7538750fe719168ae8f95
João Pedro Pereira Lima <84398388+JoaoPedroSH@users.noreply.github.com>
2026-02-08 23:19:55 -0300

    Update 20260913090115-order_of_service.js

--- src/database/migrations/20260913090115-order_of_service.js
+++ src/database/migrations/20260913090115-order_of_service.js
@@ -1,8 +1,6 @@
-'use strict' 
-
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
+export default {
+  async up(queryInterface, Sequelize) {
     await queryInterface.createTable("order_of_service", {
       tenant_id: {
         type: Sequelize.INTEGER,
@@ -37,12 +35,12 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING,
       },
-    }); 
+    });
 
     await queryInterface.addIndex("order_of_service", ["tenant_id"]);
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable("order_of_service") 
-  }
-} 
+  async down(queryInterface) {
+    await queryInterface.dropTable("order_of_service");
+  },
+};
